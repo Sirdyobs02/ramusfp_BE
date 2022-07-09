8ea995d7e2af527140e559e2830c5d186f719c24
Sirdyobs02 <108305551+Sirdyobs02@users.noreply.github.com>
2022-07-09 15:34:50 +0200

    initial commit

---
+++ src/main/java/com/playground/ramusfp/RamusfpApplication.java
@@ -0,0 +1,13 @@
+package com.playground.ramusfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamusfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamusfpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/ramusfp/RamusfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.ramusfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamusfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
